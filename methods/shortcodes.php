5e74f0670f86f9baffbf411d9177f7fdb75d2623
sam siroos <40514071+samsiroos@users.noreply.github.com>
2021-09-12 20:02:41 +0430

    Update shortcodes.php
    Sample Shortcode

--- methods/shortcodes.php
+++ methods/shortcodes.php
@@ -2,3 +2,17 @@
 /**
  * @package  WordpressBasePlugin
  */
+
+function test_shortcode ($atts){
+    $attributes = shortcode_atts(
+        array(
+            'item' => '',
+        ), 
+        $atts,'test');
+  $output = '';
+  $output .= $atts['item'];
+  
+  return $output;
+}
+
+add_shortcode( 'test_shortcode', 'test' );
